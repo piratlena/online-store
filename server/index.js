807a1a853f3430dcc781e4a7d3b0452109a1f879
Elena Brazhnikova <lenovosibirsk@mail.ru>
2022-12-02 19:29:03 +0700

    add controllers

--- server/index.js
+++ server/index.js
@@ -1,9 +1,34 @@
 require('dotenv').config()
-const express = require('express');
+const express = require('express')
+const sequelize = require('./db')
+
+const models = require('./models/models')
+
+const cors = require('cors')
+
+const router = require('./routes/index')
 
 
 const PORT = process.env.PORT || 5000
 
-const app = express();
+const app = express()
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
+
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log('Соединение с БД было успешно установлено')
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log('Невозможно выполнить подключение к БД: ', e)
+    }
+}
+
+
+start()
+
